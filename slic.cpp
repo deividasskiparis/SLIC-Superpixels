398fe0984db578a17d78b2a285824cbb86b0225e
deividasskiparis <deividasskiparis@hotmail.com>
2017-02-24 20:21:08 +0100

    This update was created in order to support my master thesis project for Semantic Face Segmentation
    Updates performed:
    
    1. Converted from OpenCV 1.x to OpenCV 2.x
    
    2. Added additional functionality:
    
    2.1 pixel_assignments() - returns a matrix indicating which SP does each pixel belong to
    
    2.2 SPneighbours() - return a list of neighbouring SP pairs

--- slic.cpp
+++ slic.cpp
@@ -1,10 +1,16 @@
+//#define _CRT_SECURE_NO_WARNINGS
+
 #include "slic.h"
 
 /*
  * Constructor. Nothing is done here.
  */
-Slic::Slic() {
-
+Slic::Slic(Mat image, int nc, int step, int ns) {
+	this->step = step;
+	this->nc = nc;
+	this->ns = ns;
+	img = image.clone();
+	init_data();
 }
 
 /*
@@ -21,10 +27,10 @@ Slic::~Slic() {
  * Output: -
  */
 void Slic::clear_data() {
-    clusters.clear();
-    distances.clear();
-    centers.clear();
-    center_counts.clear();
+    //clusters.clear();
+    //distances.clear();
+	//centers.clear();
+	//center_counts.clear();
 }
 
 /*
@@ -34,26 +40,21 @@ void Slic::clear_data() {
  * Input : The image (IplImage*).
  * Output: -
  */
-void Slic::init_data(IplImage *image) {
-    /* Initialize the cluster and distance matrices. */
-    for (int i = 0; i < image->width; i++) { 
-        vector<int> cr;
-        vector<double> dr;
-        for (int j = 0; j < image->height; j++) {
-            cr.push_back(-1);
-            dr.push_back(FLT_MAX);
-        }
-        clusters.push_back(cr);
-        distances.push_back(dr);
-    }
+void Slic::init_data() {
+	/* Initialize the cluster and distance matrices. */
+	vector<int> cr(img.rows, neutral);
+	vector<double> dr(img.rows, FLT_MAX);
+	clusters = vec2di(img.cols, cr);
+	distances = vec2dd(img.cols, dr);
+
     
     /* Initialize the centers and counters. */
-    for (int i = step; i < image->width - step/2; i += step) {
-        for (int j = step; j < image->height - step/2; j += step) {
+	for (int i = step; i < img.cols - step / 2; i += step) {
+		for (int j = step; j < img.rows - step / 2; j += step) {
             vector<double> center;
             /* Find the local minimum (gradient-wise). */
-            CvPoint nc = find_local_minimum(image, cvPoint(i,j));
-            CvScalar colour = cvGet2D(image, nc.y, nc.x);
+			Point nc = find_local_minimum(Point(i, j));
+			Scalar colour = get_Scal(nc.y, nc.x);
             
             /* Generate the center vector. */
             center.push_back(colour.val[0]);
@@ -67,6 +68,7 @@ void Slic::init_data(IplImage *image) {
             center_counts.push_back(0);
         }
     }
+
 }
 
 /*
@@ -76,7 +78,9 @@ void Slic::init_data(IplImage *image) {
  *         the pixel (CvScalar).
  * Output: The distance (double).
  */
-double Slic::compute_dist(int ci, CvPoint pixel, CvScalar colour) {
+double Slic::compute_dist(int ci, Point pixel, Scalar colour) {
+
+
     double dc = sqrt(pow(centers[ci][0] - colour.val[0], 2) + pow(centers[ci][1]
             - colour.val[1], 2) + pow(centers[ci][2] - colour.val[2], 2));
     double ds = sqrt(pow(centers[ci][3] - pixel.x, 2) + pow(centers[ci][4] - pixel.y, 2));
@@ -85,6 +89,7 @@ double Slic::compute_dist(int ci, CvPoint pixel, CvScalar colour) {
     
     //double w = 1.0 / (pow(ns / nc, 2));
     //return sqrt(dc) + sqrt(ds * w);
+
 }
 
 /*
@@ -94,15 +99,16 @@ double Slic::compute_dist(int ci, CvPoint pixel, CvScalar colour) {
  * Input : The image (IplImage*) and the pixel center (CvPoint).
  * Output: The local gradient minimum (CvPoint).
  */
-CvPoint Slic::find_local_minimum(IplImage *image, CvPoint center) {
+Point Slic::find_local_minimum(Point center) {
+
     double min_grad = FLT_MAX;
-    CvPoint loc_min = cvPoint(center.x, center.y);
+    Point loc_min = Point(center.x, center.y);
     
     for (int i = center.x-1; i < center.x+2; i++) {
         for (int j = center.y-1; j < center.y+2; j++) {
-            CvScalar c1 = cvGet2D(image, j+1, i);
-            CvScalar c2 = cvGet2D(image, j, i+1);
-            CvScalar c3 = cvGet2D(image, j, i);
+            Scalar c1 = get_Scal(j+1, i);
+			Scalar c2 = get_Scal(j, i + 1);
+			Scalar c3 = get_Scal(j, i);
             /* Convert colour values to grayscale values. */
             double i1 = c1.val[0];
             double i2 = c2.val[0];
@@ -120,7 +126,6 @@ CvPoint Slic::find_local_minimum(IplImage *image, CvPoint center) {
             }
         }
     }
-    
     return loc_min;
 }
 
@@ -131,39 +136,37 @@ CvPoint Slic::find_local_minimum(IplImage *image, CvPoint center) {
  * Input : The Lab image (IplImage*), the stepsize (int), and the weight (int).
  * Output: -
  */
-void Slic::generate_superpixels(IplImage *image, int step, int nc) {
-    this->step = step;
-    this->nc = nc;
-    this->ns = step;
+void Slic::generate_superpixels() {
+
+
     
     /* Clear previous data (if any), and re-initialize it. */
-    clear_data();
-    init_data(image);
+    //clear_data();
+    //init_data(image);
     
     /* Run EM for 10 iterations (as prescribed by the algorithm). */
     for (int i = 0; i < NR_ITERATIONS; i++) {
         /* Reset distance values. */
-        for (int j = 0; j < image->width; j++) {
-            for (int k = 0;k < image->height; k++) {
-                distances[j][k] = FLT_MAX;
-            }
-        }
-
+		vector<double> dist(img.rows, FLT_MAX);
+		distances = vec2dd(img.cols, dist);
+	
         for (int j = 0; j < (int) centers.size(); j++) {
             /* Only compare to pixels in a 2 x step by 2 x step region. */
             for (int k = centers[j][3] - step; k < centers[j][3] + step; k++) {
                 for (int l = centers[j][4] - step; l < centers[j][4] + step; l++) {
                 
-                    if (k >= 0 && k < image->width && l >= 0 && l < image->height) {
-                        CvScalar colour = cvGet2D(image, l, k);
-                        double d = compute_dist(j, cvPoint(k,l), colour);
+					if (k >= 0 && k < img.cols && l >= 0 && l < img.rows) {
+						Scalar colour = get_Scal(l, k);
+                        double d = compute_dist(j, Point(k,l), colour);
                         
                         /* Update cluster allocation if the cluster minimizes the
                            distance. */
-                        if (d < distances[k][l]) {
-                            distances[k][l] = d;
-                            clusters[k][l] = j;
-                        }
+						//cout << "d: " << d << "\tk: " << k << "\tl: " << l << endl;
+	
+						if (d < distances[k][l]) {
+							distances[k][l] = d;
+							clusters[k][l] = j;
+						}
                     }
                 }
             }
@@ -176,12 +179,12 @@ void Slic::generate_superpixels(IplImage *image, int step, int nc) {
         }
         
         /* Compute the new cluster centers. */
-        for (int j = 0; j < image->width; j++) {
-            for (int k = 0; k < image->height; k++) {
+		for (int j = 0; j < img.cols; j++) {
+			for (int k = 0; k < img.rows; k++) {
                 int c_id = clusters[j][k];
                 
-                if (c_id != -1) {
-                    CvScalar colour = cvGet2D(image, k, j);
+				if (c_id != neutral) {
+					Scalar colour = get_Scal(k, j);
                     
                     centers[c_id][0] += colour.val[0];
                     centers[c_id][1] += colour.val[1];
@@ -203,6 +206,7 @@ void Slic::generate_superpixels(IplImage *image, int step, int nc) {
             centers[j][4] /= center_counts[j];
         }
     }
+
 }
 
 /*
@@ -213,35 +217,38 @@ void Slic::generate_superpixels(IplImage *image, int step, int nc) {
  * Input : The image (IplImage*).
  * Output: -
  */
-void Slic::create_connectivity(IplImage *image) {
+void Slic::create_connectivity() {
+
     int label = 0, adjlabel = 0;
-    const int lims = (image->width * image->height) / ((int)centers.size());
+	const int lims = (img.cols * img.rows) / ((int)centers.size());
     
     const int dx4[4] = {-1,  0,  1,  0};
 	const int dy4[4] = { 0, -1,  0,  1};
     
     /* Initialize the new cluster matrix. */
-    vec2di new_clusters;
-    for (int i = 0; i < image->width; i++) { 
-        vector<int> nc;
-        for (int j = 0; j < image->height; j++) {
-            nc.push_back(-1);
-        }
-        new_clusters.push_back(nc);
-    }
+ //   vec2di new_clusters;
+	//for (int i = 0; i < img.cols; i++) {
+ //       vector<int> nc;
+	//	for (int j = 0; j < img.rows; j++) {
+	//		nc.push_back(neutral);
+ //       }
+ //       new_clusters.push_back(nc);
+ //   }
+	vector<int> nc(img.rows,neutral);
+	vec2di new_clusters(img.cols, nc);
 
-    for (int i = 0; i < image->width; i++) {
-        for (int j = 0; j < image->height; j++) {
-            if (new_clusters[i][j] == -1) {
-                vector<CvPoint> elements;
-                elements.push_back(cvPoint(i, j));
+	for (int i = 0; i < img.cols; i++) {
+		for (int j = 0; j < img.rows; j++) {
+			if (new_clusters[i][j] == neutral) {
+                vector<Point> elements;
+                elements.push_back(Point(i, j));
             
                 /* Find an adjacent label, for possible use later. */
                 for (int k = 0; k < 4; k++) {
                     int x = elements[0].x + dx4[k], y = elements[0].y + dy4[k];
                     
-                    if (x >= 0 && x < image->width && y >= 0 && y < image->height) {
-                        if (new_clusters[x][y] >= 0) {
+					if (x >= 0 && x < img.cols && y >= 0 && y < img.rows) {
+                        if (new_clusters[x][y] =! neutral) {
                             adjlabel = new_clusters[x][y];
                         }
                     }
@@ -252,9 +259,9 @@ void Slic::create_connectivity(IplImage *image) {
                     for (int k = 0; k < 4; k++) {
                         int x = elements[c].x + dx4[k], y = elements[c].y + dy4[k];
                         
-                        if (x >= 0 && x < image->width && y >= 0 && y < image->height) {
-                            if (new_clusters[x][y] == -1 && clusters[i][j] == clusters[x][y]) {
-                                elements.push_back(cvPoint(x, y));
+						if (x >= 0 && x < img.cols && y >= 0 && y < img.rows) {
+							if (new_clusters[x][y] == neutral && clusters[i][j] == clusters[x][y]) {
+                                elements.push_back(Point(x, y));
                                 new_clusters[x][y] = label;
                                 count += 1;
                             }
@@ -274,6 +281,7 @@ void Slic::create_connectivity(IplImage *image) {
             }
         }
     }
+
 }
 
 /*
@@ -282,10 +290,12 @@ void Slic::create_connectivity(IplImage *image) {
  * Input : The image to display upon (IplImage*) and the colour (CvScalar).
  * Output: -
  */
-void Slic::display_center_grid(IplImage *image, CvScalar colour) {
+void Slic::display_center_grid(Mat &image, Vec3d colour) {
+	Scalar col = Scalar(colour[0], colour[1], colour[2]);
     for (int i = 0; i < (int) centers.size(); i++) {
-        cvCircle(image, cvPoint(centers[i][3], centers[i][4]), 2, colour, 2);
+		circle(image, Point(centers[i][3], centers[i][4]), 2, col, 2);
     }
+
 }
 
 /*
@@ -294,32 +304,28 @@ void Slic::display_center_grid(IplImage *image, CvScalar colour) {
  * Input : The target image (IplImage*) and contour colour (CvScalar).
  * Output: -
  */
-void Slic::display_contours(IplImage *image, CvScalar colour) {
+void Slic::display_contours(Mat &image, Vec3d colour) {
+
     const int dx8[8] = {-1, -1,  0,  1, 1, 1, 0, -1};
 	const int dy8[8] = { 0, -1, -1, -1, 0, 1, 1,  1};
 	
 	/* Initialize the contour vector and the matrix detailing whether a pixel
 	 * is already taken to be a contour. */
-	vector<CvPoint> contours;
-	vec2db istaken;
-	for (int i = 0; i < image->width; i++) { 
-        vector<bool> nb;
-        for (int j = 0; j < image->height; j++) {
-            nb.push_back(false);
-        }
-        istaken.push_back(nb);
-    }
+	vector<Point> contours;
+	vector<bool> nb(image.rows, false);
+	vec2db istaken(image.cols, nb);
+
     
     /* Go through all the pixels. */
-    for (int i = 0; i < image->width; i++) {
-        for (int j = 0; j < image->height; j++) {
+    for (int i = 0; i < image.cols; i++) {
+        for (int j = 0; j < image.rows; j++) {
             int nr_p = 0;
             
             /* Compare the pixel to its 8 neighbours. */
             for (int k = 0; k < 8; k++) {
                 int x = i + dx8[k], y = j + dy8[k];
                 
-                if (x >= 0 && x < image->width && y >= 0 && y < image->height) {
+                if (x >= 0 && x < image.cols && y >= 0 && y < image.rows) {
                     if (istaken[x][y] == false && clusters[i][j] != clusters[x][y]) {
                         nr_p += 1;
                     }
@@ -328,7 +334,7 @@ void Slic::display_contours(IplImage *image, CvScalar colour) {
             
             /* Add the pixel to the contour list if desired. */
             if (nr_p >= 2) {
-                contours.push_back(cvPoint(i,j));
+                contours.push_back(Point(i,j));
                 istaken[i][j] = true;
             }
         }
@@ -336,44 +342,149 @@ void Slic::display_contours(IplImage *image, CvScalar colour) {
     
     /* Draw the contour pixels. */
     for (int i = 0; i < (int)contours.size(); i++) {
-        cvSet2D(image, contours[i].y, contours[i].x, colour);
+        set_Scal(image, contours[i].y, contours[i].x, colour);
     }
+
+}
+
+/*
+ * Generate matrix of pixel-wise cluster assignments
+ *
+ * Input - Target image
+ * Output - Matrix of pixel-wise allocations to cluster centres
+ *
+ */
+cv::Mat Slic::pixel_assignments(){
+
+	cv::Mat_<int> px_assign = Mat(img.rows, img.cols, CV_8UC1, Scalar(0));
+	for (int i = 0; i < px_assign.rows; ++i){
+		for (int j = 0; j < px_assign.cols; ++j){
+			//cout << "i: " << i << ", j: " << j << ", value: " << clusters.at(i).at(j) << endl;
+			int clust_id = (int)clusters[j][i];
+			px_assign(i, j) = clust_id;
+		}
+	}
+
+	return(px_assign);
 }
 
 /*
  * Give the pixels of each cluster the same colour values. The specified colour
  * is the mean RGB colour per cluster.
  *
- * Input : The target image (IplImage*).
+ * Input : The target image (Mat).
  * Output: -
  */
-void Slic::colour_with_cluster_means(IplImage *image) {
-    vector<CvScalar> colours(centers.size());
-    
+vector<cv::Vec3d> Slic::colour_with_cluster_means() {
+	vector<cv::Vec3d> colours(centers.size(), Vec3d(0, 0, 0));
+	vector<int> color_counts(centers.size(), 0);
+
     /* Gather the colour values per cluster. */
-    for (int i = 0; i < image->width; i++) {
-        for (int j = 0; j < image->height; j++) {
+	for (int i = 0; i < img.cols; i++) {
+		for (int j = 0; j < img.rows; j++) {
             int index = clusters[i][j];
-            CvScalar colour = cvGet2D(image, j, i);
-            
-            colours[index].val[0] += colour.val[0];
-            colours[index].val[1] += colour.val[1];
-            colours[index].val[2] += colour.val[2];
+			if (index != neutral){
+				Scalar colour = get_Scal(j, i);
+				colours[index].val[0] += colour.val[0];
+				colours[index].val[1] += colour.val[1];
+				colours[index].val[2] += colour.val[2];
+				color_counts[index] += 1;
+			}
         }
     }
-    
+
     /* Divide by the number of pixels per cluster to get the mean colour. */
     for (int i = 0; i < (int)colours.size(); i++) {
-        colours[i].val[0] /= center_counts[i];
-        colours[i].val[1] /= center_counts[i];
-        colours[i].val[2] /= center_counts[i];
+		int cnt = color_counts[i];
+		if (cnt == 0) continue;
+		colours[i].val[0] /= cnt;
+		colours[i].val[1] /= cnt;
+		colours[i].val[2] /= cnt;
     }
-    
+
     /* Fill in. */
-    for (int i = 0; i < image->width; i++) {
-        for (int j = 0; j < image->height; j++) {
-            CvScalar ncolour = colours[clusters[i][j]];
-            cvSet2D(image, j, i, ncolour);
+	for (int i = 0; i < img.cols; i++) {
+		for (int j = 0; j < img.rows; j++) {
+			int cluster_idx = clusters[i][j];
+			if (cluster_idx != neutral){
+				Vec3d ncolour = colours[cluster_idx];
+				set_Scal(img, j, i, ncolour);
+			}
+
         }
     }
+
+	return colours;
+}
+/* 
+ * Generate a list of neighbouring superpixels
+ * Makes a non-repeating list, meaning if 1 and 2 are neighbours the list will
+ * contain either (1,2) or (2,1) but not both
+ */
+std::vector<Vec3d> Slic::SPneighbours(cv::Mat pxl_wise_assmts){
+
+	std::vector<Vec3d> result;
+	cv::Mat_<int> sps(pxl_wise_assmts);
+	int w = pxl_wise_assmts.size().width, h = pxl_wise_assmts.size().height;
+	
+	int N_SP = get_nSP();
+	cv::Mat_<int> assimts_temp = cv::Mat::zeros(cv::Size(N_SP,N_SP), CV_8UC1);
+
+
+	for (int i = 0; i < h - 1; i++){ // for every row
+		for (int j = 0; j < w - 1; j++){ // for every column
+			//std::pair<int, int> p1(0, 0), p2(0, 0);
+			Vec3d p1(0, 0, 1), p2(0, 0, 1);
+
+			if ((i < h - 1) && (j < w - 1)){
+
+
+				int p_ref = sps(i, j), p_dx = sps(i, j + 1), p_dy = sps(i + 1, j), p_dxy = sps(i + 1, j + 1);
+				if (p_ref >= 0 && p_dx >= 0 && p_ref < N_SP && p_dx < N_SP)
+					p_ref < p_dx ? p1[0] = p_ref, p1[1] = p_dx : p1[0] = p_dx, p1[1] = p_ref;
+				if (p_ref >= 0 && p_dy >= 0 && p_ref < N_SP && p_dy < N_SP)
+					p_ref < p_dx ? p2[0] = p_ref, p2[1] = p_dy : p2[0] = p_dy, p2[1] = p_ref;
+
+					//p_ref < p_dy? p2 = std::make_pair(p_ref, p_dy) : p2 = std::make_pair(p_dy, p_ref);
+			}
+
+			if ((assimts_temp(p1[0], p1[1]) != 1) && (p1[0] != p1[1])){ // value has not been added to the list yet
+				result.push_back(p1);
+				assimts_temp(p1[0], p1[1]) = 1;
+			}
+
+			if ((assimts_temp(p2[0], p2[1]) != 1) && (p2[0] != p2[1])){ // value has not been added to the list yet
+				result.push_back(p2);
+				assimts_temp(p2[0], p2[1]) = 1;
+			}
+		}
+		
+	}
+	return result;
+}
+/*
+Get number of superpixels
+
+*/
+int Slic::get_nSP(){
+	return (int)centers.size();
+}
+
+/*
+Helper functions for opencv 1.x to opencv 2.x conversion
+*/
+Scalar Slic::get_Scal(int row, int col){
+	Scalar v = img.at<Vec3b>(row, col);
+	return v;
+}
+void Slic::set_Scal(Mat &in_img, int row, int col, Vec3d colour){
+
+	Vec3b v(colour);
+	in_img.at<Vec3b>(row, col) = v;
+
+}
+void Slic::set_param(int nc, int step, int ns){
+	this->step = step;
+	this->nc = nc;
+	this->ns = ns;
 }
--- slic.h
+++ slic.h
@@ -13,13 +13,15 @@
  * over-segmentations in an OpenCV-based environment.
  */
 
-#include <opencv/cv.h>
-#include <opencv/highgui.h>
+#include <opencv2\opencv.hpp>
 #include <stdio.h>
 #include <math.h>
 #include <vector>
 #include <float.h>
+#include <utility>
+
 using namespace std;
+using namespace cv;
 
 /* 2d matrices are handled by 2d vectors. */
 #define vec2dd vector<vector<double> >
@@ -48,31 +50,53 @@ class Slic {
         
         /* The step size per cluster, and the colour (nc) and distance (ns)
          * parameters. */
-        int step, nc, ns;
+		int step = 0, nc = 0, ns = 0;
         
         /* Compute the distance between a center and an individual pixel. */
-        double compute_dist(int ci, CvPoint pixel, CvScalar colour);
+        double compute_dist(int ci, Point pixel, Scalar colour);
         /* Find the pixel with the lowest gradient in a 3x3 surrounding. */
-        CvPoint find_local_minimum(IplImage *image, CvPoint center);
+        Point find_local_minimum( Point center);
         
         /* Remove and initialize the 2d vectors. */
         void clear_data();
-        void init_data(IplImage *image);
+        void init_data();
 
+		/* Replacement for cvGet2D function*/
+		Scalar get_Scal(int row, int col);
+		void set_Scal(Mat &img, int row, int col, Vec3d colour);
+
+		Mat img;
+		
+		int neutral = 1e4;
     public:
+		
         /* Class constructors and deconstructors. */
-        Slic();
+		Slic(Mat image, int nc, int step, int ns);
         ~Slic();
         
         /* Generate an over-segmentation for an image. */
-        void generate_superpixels(IplImage *image, int step, int nc);
+        void generate_superpixels();
         /* Enforce connectivity for an image. */
-        void create_connectivity(IplImage *image);
+        void create_connectivity();
         
         /* Draw functions. Resp. displayal of the centers and the contours. */
-        void display_center_grid(IplImage *image, CvScalar colour);
-        void display_contours(IplImage *image, CvScalar colour);
-        void colour_with_cluster_means(IplImage *image);
+        void display_center_grid(Mat &image, Vec3d colour);
+        void display_contours(Mat &image, Vec3d colour);
+		vector<cv::Vec3d> colour_with_cluster_means();
+
+		/*Generate pixel assignment Mat file*/
+		cv::Mat pixel_assignments();
+
+		/*Generate a list of neighbouring superpixels*/
+		//std::vector< std::pair<int, int> > SPneighbours(cv::Mat pxl_wise_assmts);
+		std::vector<Vec3d> SPneighbours(cv::Mat pxl_wise_assmts);
+		
+
+		/* Get number of pixels */
+		int get_nSP();
+
+		/* Set parameters for superpixelation*/
+		void set_param(int nc, int step, int ns);
 };
 
 #endif
